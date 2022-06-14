f9c305c4b714e97441f21427e5f5b52b70c5e5a3
ngomezap <igomez.ap@gmail.com>
2022-06-14 19:55:02 +0200

    Añade gui basica en funcion de la Temp

--- script.js
+++ script.js
@@ -3,13 +3,23 @@
 const btn = document.querySelector('button');
 const divData = document.getElementById('data');
 const textCity = document.getElementById('city');
+const image = document.querySelector('img');
+let temp;
 
 async function getData(city){
     try{
         let weatherData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=da24bd075527e1ea20918f419cbd4c31`);
         if(weatherData.status == 200){
             let jsData = await weatherData.json();
-            console.log(jsData);
+            temp = parseInt(jsData.main["temp"]) - 273;
+            if(temp > 20 && temp < 30){
+                image.src = "./images/cool.jpeg";
+            }else if(temp >= 30){
+                image.src = "./images/hot.jpg"
+            }else{
+                image.src = "./images/cold.png";
+            }
+            
         }else{
             console.log("uh oh");
         }
@@ -20,5 +30,5 @@ async function getData(city){
 
 
 btn.addEventListener('click', function(){
-    getData(textCity.value).catch("uh oh");
+    getData(textCity.value)
 })
