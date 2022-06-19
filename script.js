eaece0e42186dea9d87a27079ad56b608cd9c527
ngomezap <igomez.ap@gmail.com>
2022-06-19 20:06:16 +0200

    Basic funcinalities and dropdown menu

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
-
-
 const btn = document.querySelector('button');
 const divData = document.getElementById('data');
 const textCity = document.getElementById('city');
 const image = document.querySelector('img');
+const circle = document.getElementById('circle');
+const menuSelector = document.querySelectorAll('nav > div');
 let temp;
 
-async function getData(city){
-    try{
-        let weatherData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=da24bd075527e1ea20918f419cbd4c31`);
-        if(weatherData.status == 200){
-            let jsData = await weatherData.json();
-            temp = parseInt(jsData.main["temp"]) - 273;
-            if(temp > 20 && temp < 30){
-                image.src = "./images/cool.jpeg";
-            }else if(temp >= 30){
-                image.src = "./images/hot.jpg"
-            }else{
-                image.src = "./images/cold.png";
-            }
-            
-        }else{
-            console.log("uh oh");
-        }
-    }catch(err){
-        console.log("uh oh");
-    }
+function getData(city){
+    
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=da24bd075527e1ea20918f419cbd4c31`)
+    .then((resp) => {
+        return resp.json();
+    })
+    .then(function(resp){
+        circle.classList.remove("load");
+        console.log(resp);
+        divData.innerHTML = `<p>TEMP: ${parseInt(resp.main.temp)-273}ºC</p>
+        <p>${resp.weather[0].description.toUpperCase()}</p>`;
+    });
 }
 
-
 btn.addEventListener('click', function(){
-    getData(textCity.value)
+    divData.innerHTML = "";
+    circle.classList.add("load");
+    setTimeout(function(){
+        getData(textCity.value);
+    }, 2000);
 })
+
+
+menuSelector.forEach(n => {
+    n.addEventListener("mouseover", () => {
+        document.querySelector('nav').childNodes.forEach((n) => {
+            if(n.nodeName === 'DIV'){
+                n.classList.remove('menuOpt');
+            }
+        })
+    })
+    
+    n.addEventListener("mouseleave", () => {
+        document.querySelectorAll("nav > div:not(#menu)").forEach((n) => {
+            if(n.nodeName === 'DIV'){
+                n.classList.add('menuOpt');
+            }
+        })
+    })
+})
+
+
+
